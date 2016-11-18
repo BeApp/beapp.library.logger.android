ce2cf487e3f6b349aebb940917edbd3007b90b2f
Damien Villeneuve <d.villeneuve@beapp.fr>
2016-11-18 14:51:18 +0100

    Minor update on unit test

--- logger/src/test/java/fr/beapp/logger/LoggerTest.java
+++ logger/src/test/java/fr/beapp/logger/LoggerTest.java
@@ -8,43 +8,58 @@ import android.util.Log;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import fr.beapp.logger.appender.Appender;
+import fr.beapp.logger.formatter.Formatter;
 
 public class LoggerTest {
 
 	private static class DummyAppender extends Appender {
 		private final Integer key;
-		private final Map<Integer, Boolean> acc;
+		private final Map<Integer, List<String>> acc;
 
-		private DummyAppender(Integer key, Map<Integer, Boolean> acc) {
+		private DummyAppender(Integer key, Map<Integer, List<String>> acc) {
 			this.key = key;
 			this.acc = acc;
 		}
 
 		@Override
 		public void log(@Logger.LogLevel int priority, @NonNull String message, @Nullable Throwable tr) {
-			acc.put(key, true);
+			List<String> strings = acc.get(key);
+			if (strings == null) {
+				strings = new ArrayList<>();
+				acc.put(key, strings);
+			}
+			strings.add(message);
 		}
 	}
 
 	@Test
 	@SuppressLint("UseSparseArrays")
 	public void log() throws Exception {
-		Map<Integer, Boolean> acc = new HashMap<>();
+		Map<Integer, List<String>> acc = new HashMap<>();
 
 		DummyAppender appender1 = new DummyAppender(1, acc);
 		DummyAppender appender5 = new DummyAppender(5, acc);
 
 		Logger.add(appender1);
 		Logger.add(appender5);
+		Logger.formatter(new Formatter() {
+			@Nullable
+			@Override
+			public String format(@Nullable Throwable tr, @Nullable String message, Object... args) {
+				return "formatted:" + message;
+			}
+		});
 
 		Logger.log(Log.DEBUG, null, "test message");
 
-		Assert.assertEquals(true, acc.get(1));
-		Assert.assertEquals(true, acc.get(5));
+		Assert.assertArrayEquals(new String[]{"formatted:test message"}, acc.get(1).toArray());
+		Assert.assertArrayEquals(new String[]{"formatted:test message"}, acc.get(5).toArray());
 	}
 
 }
